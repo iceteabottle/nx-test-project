b376c06f78cd14a5304b71908b2dfe4e8e854ed0
Florian Symanowski <f.symanowski@ewerk.com>
2023-02-22 09:30:10 +0100

    Fix the issue finally

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,16 @@
 const nxPreset = require('@nrwl/jest/preset').default;
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  /**
+   * BE AWARE!
+   *
+   * The following entries are needed to run jest natively (e.g. plugin in IntelliJ) because jest doesn't understand webpack's path aliases.
+   * If you've trouble with failing tests ONLY in IntelliJ try to add the path mapping for the corresponding lib.
+   *
+   * See https://gitlab.com/gitlab-org/gitlab/-/issues/299576
+   */
+  moduleNameMapper: {
+    '@nx-test-project/my-lib/(.*)$': '<rootDir>/../../libs/my-lib/src/$1',
+  }
+};
